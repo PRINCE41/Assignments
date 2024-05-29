856cc3d49d516053d03b5e6101d0ebacc3ca9393
PawanPrasoonMishra <pawanprasoonmishra@gmail.com>
2024-05-29 14:35:11 +0530

    tcs

---
+++ tcs/RevSorting.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RevSorting{
+
+    public static void main(String[] args) {
+        int[] givenNo = {3, 5, 7, 8, 9, 0, 1, 2, 3};
+
+        List<Integer> list = Arrays
+            .stream(givenNo)
+            .boxed()
+            .sorted((a, b) -> b - a)
+            .collect(Collectors.toList());
+
+        list.forEach(x -> {
+            System.out.println(x);
+        });
+    }
+
+}
